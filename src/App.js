415d30e9b7e89149913197936e909f26dfb952a2
agustinmfran <agustinmfran@gmail.com>
2023-01-08 19:48:14 +0100

    using formik components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import {useFormik} from 'formik'
+import {Formik, Form, Field, ErrorMessage} from 'formik'
+
+const validate = (values) =>{
+  const errors = {}
+  if(!values.name){ 
+    errors.name = 'Requerido'
+  } else if (values.name.length < 5){
+      errors.name = 'El nombre es muy corto'
+  }
+
+  if(!values.lastname){ 
+    errors.lastname = 'Requerido'
+  } else if (values.lastname.length < 5){
+      errors.lastname = 'El apellido es muy corto'
+  }
+  
+  return errors
+}
 
 function App() {
-  const formik = useFormik({
-    initialValues:{
-      name: '',
-      lastname: '',
-      email: '',
-    },
-    onSubmit: values => console.log(values)
-  })
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label>Nombre</label>
-      <input
-      name='name'
-      type='text'
-      onChange={formik.handleChange}
-      value={formik.values.name}
-      />
-      <label>Apellido</label>
-      <input
-      name='lastname'
-      type='text'
-      onChange={formik.handleChange}
-      value={formik.values.lastname}
-      />
-      <label>Email</label>
-      <input
-      name='email'
-      type='email'
-      onChange={formik.handleChange}
-      value={formik.values.email}
-      />
-      <button type='submit'>Enviar</button>      
-    </form>
+    <Formik
+        initialValues={{name: '', lastname: '', email: ''}}
+        validate={validate}
+        onSubmit={values => console.log(values)}
+    >
+      <Form>
+        <label>Nombre</label>
+        <Field name='name' type='text'/>
+        <ErrorMessage name='name' />
+        <br/>
+        <label>Apellido</label>
+        <Field name='lastname' type='text' />
+        <ErrorMessage name='lastname' />
+        <br/>
+        <label>Email</label>
+        <Field name='email' type='email' />
+        <ErrorMessage name='email' />
+        <button type='submit'>Enviar</button>      
+      </Form>
+    </Formik>
   );
 }
 
